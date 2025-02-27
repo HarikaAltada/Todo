dfa7c86e63dda8e3032b3faa78f30e9a466130b3
HarikaAltada <151178873+HarikaAltada@users.noreply.github.com>
2025-02-27 18:36:26 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -16,6 +16,9 @@ app.use(express.json());
 
 // Routes
 app.use("/api", taskRoutes);
+app.get("/", (req, res) => {
+    res.send("Welcome to the blog API");
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
